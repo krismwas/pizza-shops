f07082e23a1cfe58fa0d2846dc5d364d0f50508e
kris <krismwasgit@gmail.com>
2025-01-30 16:53:29 +0300

    app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">fast pizza</Link>
+      <p>Chris</p>
+    </header>
+  );
+}
+
+export default Header;
