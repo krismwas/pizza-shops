6a9b26672b65067d03207eaac2a20a10de3c492a
kris <krismwasgit@gmail.com>
2025-02-05 17:42:03 +0300

    styling text

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">fast pizza</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        fast pizza
+      </Link>
+
       <SearchOrder />
+
+      <Username />
     </header>
   );
 }
