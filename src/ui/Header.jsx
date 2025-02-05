c53dea4295e219181c08a5b5255b2e8e021b0d12
kris <krismwasgit@gmail.com>
2025-02-05 18:07:29 +0300

    spacing content

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="py-uppercase border border-b border-stone-200 bg-yellow-500 px-4 py-3">
       <Link to="/" className="tracking-widest">
         fast pizza
       </Link>
