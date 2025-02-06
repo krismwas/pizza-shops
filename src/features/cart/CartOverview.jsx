16f69e4f2a5222e0791f3107424011038cd89c96
kris <krismwasgit@gmail.com>
2025-02-06 12:01:40 +0300

    added flexbox

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function CartOverview() {
   return (
-    <div className="bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6">
+    <div className="items flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6">
       <p className="space-x-4 text-sm font-semibold text-stone-300 md:text-base">
         <span>23 pizzas</span>
         <span>$23.45</span>
